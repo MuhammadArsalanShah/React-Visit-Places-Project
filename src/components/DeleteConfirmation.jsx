73aa3adcd83a2e52b2912aff5281e2de04653c73
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-09-06 16:39:25 +0500

    Adding a remaining time progress bar for delete confirmation modal and using useEffect to manage it

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,30 +1,55 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
+const TIMER = 5000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
   useEffect(() => {
-    console.log('Timer Set.');
-    const timer = setTimeout(() => {
-      onConfirm();
-    }, 5000);
+    const interval = setInterval(() => {
+      console.log("Interval running...");
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
 
     return () => {
-      console.log('Cleaned timer...')
-      clearTimeout(timer)
-    }
+      console.log("Cleaned interval...");
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("Timer Set.");
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, TIMER);
+
+    return () => {
+      console.log("Cleaned timer...");
+      clearTimeout(timer);
+    };
   }, [onConfirm]);
-  
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
       <p>Do you really want to remove this place?</p>
       <div id="confirmation-actions">
-        <button onClick={onCancel} className="button-text">
-          No
-        </button>
-        <button onClick={onConfirm} className="button">
-          Yes
-        </button>
+        <div id="progress-container">
+          <label htmlFor="progress-bar">Self Deletion Remaining Time:</label>
+          <progress id="progress-bar" value={remainingTime} max={TIMER} />
+        </div>
+        <div>
+          <button onClick={onCancel} className="button-text">
+            No
+          </button>
+          <button
+            style={{ marginLeft: "1rem" }}
+            onClick={onConfirm}
+            className="button"
+          >
+            Yes
+          </button>
+        </div>
       </div>
     </div>
   );
