367f6f8fc156e536e1f234890824f572caa3d663
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-09-06 13:07:47 +0500

    Creating another useEffect usage scenarion inside Delete confirmation function

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,12 @@
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+
+  console.log('Delete Confirmation Set');
+
+  setTimeout(() => {
+    console.log('in timer functions...');
+    onConfirm();
+  }, 3000);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
