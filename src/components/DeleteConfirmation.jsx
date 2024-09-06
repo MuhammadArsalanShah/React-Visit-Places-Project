b11b4d4cf6ddbe6a57722f8ac171c219ae4e2672
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-09-06 16:56:09 +0500

    Finishing this section

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import ProgressBar from "./ProgressBar.jsx";
 
-const TIMER = 5000;
+const TIMER = 7000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
 
